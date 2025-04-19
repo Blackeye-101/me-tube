f99ae6c24455553db5a6683551b978147c3f7738
unknown <rishabh.th25@gmail.com>
2025-04-19 19:02:36 +0530

    adding the video schema, create video route and view videos section

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,13 +1,28 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { Loader2, PlusIcon, CheckCircleIcon, XCircleIcon } from "lucide-react"
+import { trpc } from "@/trpc/client"
+import { toast } from "sonner"
 
 export const StudioUploadModal = () => {
+  const utils=trpc.useUtils()
+  const create= trpc.videos.create.useMutation({
+    onSuccess: () => {
+    toast("Video uploaded successfully!", {
+      icon: <CheckCircleIcon className="text-green-600" />,
+    })
+    utils.studio.getMany.invalidate()
+  },
+  onError: () => {
+    toast("Oops! Something went wrong", {
+      icon: <XCircleIcon className="text-red-600" />,
+    })
+  }
+  })
   return (
-    <Button variant="secondary">
-        <PlusIcon>
-        </PlusIcon>
+    <Button variant="secondary" onClick={()=>create.mutate()} disabled={create.isPending}>
+        {create.isPending?<Loader2 className="animate-spin"></Loader2>:<PlusIcon></PlusIcon>}
         Create
     </Button>
   )
